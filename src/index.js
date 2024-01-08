24bbabfb41d9882fd354c48b60468a04b0106590
ashein-codethedreams <heroglasses155@gmail.com>
2024-01-08 10:11:20 +0630

    init languages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import translationEN from "./public/locales/en.json";
+import translationMm from "./public/locales/mm.json";
+
+const resources = {
+  en: { translation: translationEN },
+  es: { translation: translationMm },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "en",
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
